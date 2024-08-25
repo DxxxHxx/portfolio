8b4fbd6bfed265a8d18c30b303414466dc291486
DxxxHxx <donghun1163@gmail.com>
2024-08-25 18:33:33 +0900

    Refactor: Global style 작성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const GlobalStyle = createGlobalStyle`html,
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+  font-family: "Noto Sans KR", sans-serif;
+  height: 300vh;
+  background-color: #f3f3f3;
+  /* padding: 0px 10px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0px 30px;
+  } */
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+  outline: none;
+}
+`;
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
