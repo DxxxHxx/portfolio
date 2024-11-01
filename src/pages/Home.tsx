98dc8286b847c13e2fa1522d55bc8ad8dd707930
DxxxHxx <donghun1163@gmail.com>
2024-11-01 16:53:08 +0900

    Feat: 누락 컴포넌트,스타일링 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,12 @@ import About from "../components/about/About";
 import Skills from "../components/skills/Skills";
 import Projects from "../components/projects/Projects";
 import Contact from "../components/contact/Contact";
+import ProgressBar from "../components/common/ProgressBar";
 
 export default function Home() {
   return (
     <>
+      <ProgressBar />
       <Landing />
       <About />
       <Skills />
